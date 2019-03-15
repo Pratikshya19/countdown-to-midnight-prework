b3fdced50e99dd8e990222d9dba840e32ae19ca9
Pratikshya Pandey <pratikshya.pp19@gmail.com>
2019-03-15 18:38:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(number)
+while number > 0
+puts "#{number} SECOND(S) !"
+number -= 1
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+  puts "#{seconds_to_midnight} SECONDS(S) !"
+  sleep(1)
+  number -= 1
+end
+"HAPPY NEW YEAR!"
 end
